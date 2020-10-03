f94c8cbe24468ca38ec624a62f6a0f0dc019eb83
Max Dickinson <maxsantistadickinson@gmail.com>
2020-10-03 16:55:42 -0300

    Criando projeto e estrutura inicial.

---
+++ src/pages/Estagio/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Estagio() {
+    return (
+        <Container>
+            <h3>Página do estágio</h3>
+        </Container>
+    )
+}
