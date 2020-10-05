533fedf885f94035c4efe8f873abee8c59a83b48
Keven Max <kmax136@gmail.com>
2020-10-05 15:58:15 -0300

    Add divider in Module container component

--- src/components/ModuleContainer/index.js
+++ src/components/ModuleContainer/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Divider } from 'antd';
+
 import { Container, Title } from "./styles";
+
 
 export default function ModuleContainer({ children, title }) {
   return (
     <Container>
       <Title>
         <h1>{title}</h1>
+        <Divider />
       </Title>
       {children}
     </Container>
